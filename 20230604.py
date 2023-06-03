e70077de639ceed43886946f6075c0c8e0697508
mok <lerosm@outlook.com>
2023-06-04 00:16:58 +0800

    test6

--- 20230604.py
+++ 20230604.py
@@ -2,7 +2,6 @@ import requests
 session = requests.session()
 a = session.get("http://www.baidu.com")
 #print("hello world")
-print(a.content.decode('utf-8'))
 #print(a)
 print("hello world")
 print('123')
